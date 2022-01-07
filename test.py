d55bb73883e788fb634e9fd2447c11e6aec51e3a
kschen405 <terryksc@gmail.com>
2022-01-08 01:55:27 +0800

    successfully fill all texts and options

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 from selenium.webdriver.common.by import By
+from random import randrange
 
 option = webdriver.ChromeOptions()
 option.add_argument('disable-infobars')
@@ -14,31 +15,35 @@ option.add_experimental_option("prefs", {
 url = "https://docs.google.com/forms/d/1FEITIpeubZ-K8X7oBWJ2vNgzadfiLZJ6fotS2sdNCos/viewform?edit_requested=true"
 driver = webdriver.Chrome(options=option)
 driver.get(url)
-option = driver.find_elements_by_class_name(
-    'appsMaterialWizToggleRadiogroupEl')
-
-
-option_status = option[2]
-option_status.click()
-
-'''
-nextbtn = driver.find_elements_by_class_name('appsMaterialWizButtonEl')[1]
-nextbtn.click()
-option = driver.find_elements_by_class_name(
-    'appsMaterialWizToggleRadiogroupEl')
-
-option_status = option[1]
-option_status.click()
-'''
+'''options = driver.find_elements_by_class_name(
+    'quantumWizTextinputPaperinputInput')
+for option in options:
+    print(option)
+    print("\n")'''
+option_num = ['7', '11', '12', '14', '15', '16']
+for i in range(6):
+    options = driver.find_element_by_xpath(
+        f'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[{option_num[i]}]')
+    options = options.find_elements_by_class_name(
+        "appsMaterialWizToggleRadiogroupEl")
+    options[randrange(len(options))].click()
+# //*[@id="mG61Hd"]/div[2]/div/div[2]/div[11]
+# //*[@id="mG61Hd"]/div[2]/div/div[2]/div[12]
 
 input = driver.find_elements_by_class_name(
     'quantumWizTextinputPaperinputInput')
 
-input_array = ["map", "email.com", "myname",
-               "phone num", "2502", "785225", "7278587", "8532572", "none", "OHHHHHHHH", "test"]
+input_array = ["youremail", "schoolmap", "somehow youremail again", "yourname",
+               "phonenum", "2050", "yourschool", "test", "test", "test", "test", "test"]
 for i in range(len(input)):
     input[i].clear()
     input[i].send_keys(input_array[i])
 
-time.sleep(10)
+submit = driver.find_elements_by_xpath(
+    '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[3]/div[1]/div/span/span')
+
+print(submit)
+submit[0].click()
+
+time.sleep(3)
 driver.quit()
